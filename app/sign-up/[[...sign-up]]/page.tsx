b531123259ef335bd441d29ded5f9dda107e30e2
Mackson Ncube <randomplayables@proton.me>
2025-03-18 23:47:38 +0000

    work on sign-in and signed-in functionality

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function SignUpPage() {
+    return (
+        <div>
+            {" "}
+            <SignUp/>
+            {" "}
+        </div>
+    )
+}
