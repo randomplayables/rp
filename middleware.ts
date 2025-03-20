22587967fa09a49ffd1a3262a3ded795c0ca8068
Mackson Ncube <randomplayables@proton.me>
2025-03-20 23:55:16 +0000

    setup middleware and neon database

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,30 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-up(.*)",
+  "/subscribe(.*)"
+])
+
+const isSignUpRoute = createRouteMatcher(["/sign-up(.*)"])
+
+export default clerkMiddleware(async (auth, req) => {
+  const userAuth = await auth()
+  const { userId } = userAuth
+  const { pathname, origin } = req.nextUrl
+  console.log("Middleware Infor: ", userId, pathname, origin)
+
+  if (!isPublicRoute(req) && !userId) {
+    return NextResponse.redirect(new URL("/sign-up", origin))
+  }
+
+  if (isSignUpRoute(req) && userId) {
+    return NextResponse.redirect(new URL("/mealplan", origin))
+  }
+
+  return NextResponse.next()
+});
 
 export const config = {
   matcher: [
