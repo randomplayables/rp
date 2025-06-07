cdbbe19e1139baa744a74c91a9e07f726d8cb59f
Mackson Ncube <randomplayables@proton.me>
2025-06-07 20:24:34 +0000

    remove dead code

--- lib/corsConfig.ts
+++ lib/corsConfig.ts
@@ -3,8 +3,8 @@ export const allowedOrigins = [
     'http://localhost:3000',         // Next.js dev server
     'http://172.31.12.157:5173',     // EC2 Vite dev server
     'http://172.31.12.157:3000',     // EC2 Next.js dev server
-    'http://3.101.155.143:5173',      // EC2 public IP Vite dev server
-    'http://3.101.155.143:3000',      // EC2 public IP Next.js dev server
+    'http://18.144.27.119:5173',      // EC2 public IP Vite dev server
+    'http://18.144.27.119:3000',      // EC2 public IP Next.js dev server
     'https://randomplayables.com',
     'https://gothamloops.randomplayables.com'
   ];
