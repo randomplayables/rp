a71235cdc9c9448981950c0eed02695706169db2
Mackson Ncube <randomplayables@proton.me>
2025-07-01 14:36:02 +0000

    remove dead code

--- types/Game.ts
+++ types/Game.ts
@@ -1,22 +1,3 @@
-// export interface IGame {
-//   gameId: string;
-//   image: string;
-//   name: string;
-//   year: number;
-//   link: string;
-//   description?: string; // Added this line
-//   version?: string;
-//   irlInstructions?: { title: string; url: string }[];
-//   codeUrl?: string;
-//   authorUsername?: string;
-//   aiUsageDetails?: {
-//     modelType: string;
-//     isPaid: boolean;
-//   };
-// }
-
-
-
 export interface IGame {
   gameId: string;
   image: string;
