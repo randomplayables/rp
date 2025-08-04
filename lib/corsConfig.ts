802e77caae19944cd8c644046d9b3dfa74ea0ede
Mackson Ncube <randomplayables@proton.me>
2025-08-04 20:59:52 +0000

    remove logs in production

--- lib/corsConfig.ts
+++ lib/corsConfig.ts
@@ -3,8 +3,8 @@ export const allowedOrigins = [
     'http://localhost:3000',         // Next.js dev server
     'http://172.31.12.157:5173',     // EC2 Vite dev server
     'http://172.31.12.157:3000',     // EC2 Next.js dev server
-    'http://54.193.31.19:5173',      // EC2 public IP Vite dev server
-    'http://54.193.31.19:3000',      // EC2 public IP Next.js dev server
+    'http://54.193.53.38:5173',      // EC2 public IP Vite dev server
+    'http://54.193.53.38:3000',      // EC2 public IP Next.js dev server
     'https://randomplayables.com',
     'https://gothamloops.randomplayables.com',
     "https://all-baboons-check.loca.lt"
